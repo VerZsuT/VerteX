bffb9a14818010eeea2e8bda06beac575c25de51
VerZsuT <sacha3032104@gmail.com>
2020-01-22 20:30:25 +0300

    Никаких изменений.

---
+++ VerteX/Lexing/Token.cs
@@ -0,0 +1,23 @@
+﻿namespace VerteX.Lexing
+{
+    public class Token
+    {
+        public TokenType type;
+        public string value;
+
+        public Token(TokenType type, string value)
+        {
+            this.type = type;
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            if (type == TokenType.String)
+            {
+                return $"\"{value}\"";
+            }
+            return value;
+        }
+    }
+}
