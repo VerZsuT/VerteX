91680b0a0fa603bd0197ea0ca042008a1816a728
VerZsuT <sacha3032104@gmail.com>
2020-01-22 20:33:23 +0300

    Добавил файл с исключениями парсера.

---
+++ VerteX/Parsing/ParseExceptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VerteX.Parsing.ParseExeptions
+{
+    public class LineParsingException : Exception
+    {
+        public LineParsingException(int lineIndex)
+        {
+            Console.WriteLine($"VerteX[ParserError]: Не удалось распознать строку {lineIndex}.");
+        }
+    }
+}
