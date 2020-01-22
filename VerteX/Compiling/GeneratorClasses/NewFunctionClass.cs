e3bb4368d539eb6ab2f2c6c7bdefb89b1f184a75
VerZsuT <sacha3032104@gmail.com>
2020-01-22 20:38:47 +0300

    Добавил хранилище кода создания новых функций.

---
+++ VerteX/Compiling/GeneratorClasses/NewFunctionClass.cs
@@ -0,0 +1,12 @@
+﻿namespace VerteX.Compiling.GeneratorClasses
+{
+    public class NewFunctionClass : ClassOfMethods
+    {
+        public void Create()
+        {
+            CodeGenerator.UserMethods.Add(name, header + string.Concat(code) + footer);
+            code.Clear();
+            variables.Clear();
+        }
+    }
+}
