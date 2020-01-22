8b7d8c67a1468642b473817a613fa96daee3b0c9
VerZsuT <sacha3032104@gmail.com>
2020-01-22 20:38:00 +0300

    Добавил хранилище кода класса UserMethods.
    Этот класс хранит пользовательские методы.

---
+++ VerteX/Compiling/GeneratorClasses/UserMethodsClass.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace VerteX.Compiling.GeneratorClasses
+{
+    public class UserMethodsClass : ClassOfMethods
+    {
+        private List<string> methods = new List<string>();
+
+        public UserMethodsClass()
+        {
+            header = TransformClassHeader("public static class UserMethods");
+            footer = GetClassFooter();
+        }
+
+        public void Add(string name, string code)
+        {
+            methods.Add(code);
+            CodeGenerator.namespaces["UserMethods"].Add(name);
+        }
+
+        public override string ToString()
+        {
+            if (methods.Count != 0)
+            {
+                string methods = string.Concat(this.methods);
+                return header + methods + footer;
+            }
+            return "";
+        }
+    }
+}
